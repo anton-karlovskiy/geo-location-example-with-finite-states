6f5978e1cb77f58ac412cc1f41457c164b4e0b7e
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2020-11-06 13:58:42 +0300

    [feat] example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
+
+import React from 'react';
+
 import './App.css';
+
+function geoPositionReducer(state, action) {
+  switch (action.type) {
+    case 'error': {
+      return {
+        ...state,
+        status: 'rejected',
+        error: action.error,
+      }
+    }
+    case 'success': {
+      return {
+        ...state,
+        status: 'resolved',
+        position: action.position,
+      }
+    }
+    case 'started': {
+      return {
+        ...state,
+        status: 'pending',
+      }
+    }
+    default: {
+      throw new Error(`Unhandled action type: ${action.type}`)
+    }
+  }
+}
+
+function useGeoPosition() {
+  const [state, dispatch] = React.useReducer(geoPositionReducer, {
+    status: 'idle',
+    position: null,
+    error: null,
+  })
+
+  React.useEffect(() => {
+    if (!navigator.geolocation) {
+      dispatch({
+        type: 'error',
+        error: new Error('Geolocation is not supported'),
+      })
+      return
+    }
+    dispatch({type: 'started'})
+    const geoWatch = navigator.geolocation.watchPosition(
+      position => dispatch({type: 'success', position}),
+      error => dispatch({type: 'error', error}),
+    )
+    return () => navigator.geolocation.clearWatch(geoWatch)
+  }, [])
+
+  return state
+}
+
+function YourPosition() {
+  const {status, position, error} = useGeoPosition()
+
+  if (status === 'idle' || status === 'pending') {
+    return <div>Loading your position...</div>
+  }
+
+  if (status === 'resolved') {
+    return (
+      <div>
+        Lat: {position.coords.latitude}, Long: {position.coords.longitude}
+      </div>
+    )
+  }
+
+  if (status === 'rejected') {
+    return (
+      <div>
+        <div>Oh no, there was a problem getting your position:</div>
+        <pre>{error.message}</pre>
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <YourPosition />
     </div>
   );
 }
