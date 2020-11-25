61fa213813ea3deedfaa8cd72a246a0037986996
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2020-11-25 13:16:39 +0300

    [feat] state machines

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@
 import React from 'react';
 
 import YourPositionByEnum from 'components/YourPositionByEnum';
+import YourPositionByXstate from 'components/YourPositionByXstate';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
+      <h2>Enum</h2>
       <YourPositionByEnum />
+      <h2>State machine</h2>
+      <YourPositionByXstate />
     </div>
   );
 }
